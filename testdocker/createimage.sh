70e5debab09c258cfa34c857b6bb69c3defff52b
Matthias Raab <m.raab@intershop.de>
2020-06-04 10:17:55 +0200

    change configuration for test docker image (2)

--- testdocker/createimage.sh
+++ testdocker/createimage.sh
@@ -5,6 +5,6 @@ echo Building base image intershopmock/icm-as-mock:latest
 USER=`whoami`
 USERID=`id -u $USER`
 
-echo Build for user $USER and id $USERID and group id $USERID
+echo Build for user $USER and id $USERID
 
 docker build --build-arg USERNAME=$USER --build-arg USERID=$USERID -t intershopmock/icm-as-mock:latest . -f ./Dockerfile
