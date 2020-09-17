ae9d96e901204f6ee20409da2d5a0a829f24e5cd
Matthias Raab <m.raab@intershop.de>
2020-09-17 21:01:56 +0200

    Message log is unmodified

--- src/main/kotlin/com/intershop/gradle/icm/docker/tasks/utils/DBPrepareCallback.kt
+++ src/main/kotlin/com/intershop/gradle/icm/docker/tasks/utils/DBPrepareCallback.kt
@@ -91,13 +91,15 @@ class DBPrepareCallback (
     }
 
     private fun getMessageString(input: String): String? {
+        /**
         val s1 = input.indexOf(MESSAGELOG_START)
         val start = s1 + MESSAGELOG_START.length + 3
         val substr = if(s1 > -1 && start < input.length) { input.substring(start) } else { input }
 
         val e1 = substr.indexOf(MESSAGELOG_END)
         val out = if(e1 > -1 && e1 - 3 < substr.length) { substr.substring(0, e1 - 3) } else { substr }
-
+        */
+        val out = input
         return if(out.trim().isNotEmpty()) {
             out.trim()
         } else {
