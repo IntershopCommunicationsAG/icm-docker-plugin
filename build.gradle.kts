993f5975834203639e474cc0f13306e380c08d1c
Sven Kiesewetter <skiesewetter@intershop.com>
2025-10-20 14:47:23 +0200

    chore: update to gradle9 (#109205)
    * also updated
      * plugin com.gradle.enterprise:3.18.1 -> 4.1.1
      * plugin kotlin("jvm"):1.9.25 -> 2.2.20
      * plugin org.asciidoctor.jvm.convert:4.0.3 -> 4.0.5
      * plugin com.gradle.plugin-publish:1.3.0 -> 2.0.0
      * java tool chain:17 -> 21
      * com.intershop.gradle.test:test-gradle-plugin:5.1.0 -> 6.0.0
      * org.apache.solr:solr-solrj:9.7.0 -> 9.9.0
      * com.intershop.gradle.icm:icm-gradle-plugin:6.2.1 -> 7.0.0
      * com.intershop.gradle.jobrunner:icmjobrunner:2.0.1 -> 3.0.0
      * actions from .github to v4
    * replaced
      * plugin org.jetbrains.dokka:1.9.20 -> org.jetbrains.dokka-javadoc:2.0.0
    * fixed
      * some kotlin incompatibilities
      * gradle 9 deprecated code calls
      * OS dependent code in ICMDockerPluginIntegrationSpec.groovy

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     `java-gradle-plugin`
     groovy
 
-    kotlin("jvm") version "1.9.25"
+    kotlin("jvm") version "2.2.20"
 
     // test coverage
     jacoco
@@ -39,13 +39,13 @@ plugins {
     signing
 
     // plugin for documentation
-    id("org.asciidoctor.jvm.convert") version "4.0.3"
+    id("org.asciidoctor.jvm.convert") version "4.0.5"
 
     // documentation
-    id("org.jetbrains.dokka") version "1.9.20"
+    id("org.jetbrains.dokka-javadoc") version "2.0.0"
 
     // plugin for publishing to Gradle Portal
-    id("com.gradle.plugin-publish") version "1.3.0"
+    id("com.gradle.plugin-publish") version "2.0.0"
 
     id("io.gitee.pkmer.pkmerboot-central-publisher") version "1.1.1"
 }
@@ -120,7 +120,7 @@ java {
     withJavadocJar()
     withSourcesJar()
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -133,14 +133,14 @@ testing {
     suites.withType<JvmTestSuite> {
         useSpock()
         dependencies {
-            implementation("com.intershop.gradle.test:test-gradle-plugin:5.1.0")
+            implementation("com.intershop.gradle.test:test-gradle-plugin:6.0.0")
             implementation(gradleTestKit())
         }
 
         targets {
             all {
                 testTask.configure {
-                    systemProperty("intershop.gradle.versions", "8.5,8.10.2")
+                    systemProperty("intershop.gradle.versions", "8.5,8.10.2,9.1.0")
                     testLogging {
                         showStandardStreams = true
                     }
@@ -211,16 +211,11 @@ tasks {
             html.outputLocation.set( project.layout.buildDirectory.dir("jacocoHtml"))
         }
 
-        val jacocoTestReport by tasks
-        jacocoTestReport.dependsOn(test)
+        dependsOn(test)
     }
     
     jar.configure {
         dependsOn(asciidoctor)
-    }
-
-    dokkaJavadoc.configure {
-        outputDirectory.set(project.layout.buildDirectory.dir("dokka"))
     }
 
     withType<Sign> {
@@ -234,9 +229,9 @@ tasks {
     }
 
     afterEvaluate {
-        named<Jar>("javadocJar") {
-            dependsOn(dokkaJavadoc)
-            from(dokkaJavadoc)
+        getByName<Jar>("javadocJar") {
+            dependsOn(dokkaGenerate)
+            from(dokkaGeneratePublicationJavadoc)
         }
     }
 }
@@ -320,9 +315,9 @@ dependencies {
     implementation(gradleApi())
     implementation(gradleKotlinDsl())
 
-    implementation("org.apache.solr:solr-solrj:9.7.0")
+    implementation("org.apache.solr:solr-solrj:9.9.0")
     implementation("com.bmuschko.docker-remote-api:com.bmuschko.docker-remote-api.gradle.plugin:9.4.0")
-    implementation("com.intershop.gradle.icm:icm-gradle-plugin:6.2.1")
-    implementation("com.intershop.gradle.jobrunner:icmjobrunner:2.0.1")
+    implementation("com.intershop.gradle.icm:icm-gradle-plugin:7.0.0")
+    implementation("com.intershop.gradle.jobrunner:icmjobrunner:3.0.0")
 }
 
