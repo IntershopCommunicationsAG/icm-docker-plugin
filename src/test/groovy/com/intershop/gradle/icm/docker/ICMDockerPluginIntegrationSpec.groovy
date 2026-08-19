b65f5e7826c61d6924c9a38e7a3a71b89309145f
Steffen Moldenhauer <s.moldenhauer@intershop.de>
2026-08-19 08:41:14 +0200

    test: upgrade referenced solr/zookeeper images (#132)

--- src/test/groovy/com/intershop/gradle/icm/docker/ICMDockerPluginIntegrationSpec.groovy
+++ src/test/groovy/com/intershop/gradle/icm/docker/ICMDockerPluginIntegrationSpec.groovy
@@ -245,8 +245,8 @@ class ICMDockerPluginIntegrationSpec extends AbstractIntegrationGroovySpec {
                 images {
                     mssqldb = 'mcr.microsoft.com/mssql/server:2019-CU4-ubuntu-16.04'
                     
-                    solr = 'solr:8.5.2-slim'
-                    zookeeper = 'zookeeper:3.6.2'
+                    solr = 'solr:9.10.1-slim'
+                    zookeeper = 'zookeeper:3.8.6'
                 }
 
                 ishUnitTests {
